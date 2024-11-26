80b7039143b387053c9de54d4fc4a0b132e26c24
Manuel Foulkes <manuel.foulkes90@gmail.com>
2024-11-26 15:11:26 -0300

    First commit

---
+++ src/main/java/com/store/StoreSalesSystemApplication.java
@@ -0,0 +1,15 @@
+package com.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreSalesSystemApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(StoreSalesSystemApplication.class, args);
+		System.out.println("Running from Store-sales system");
+	}
+
+}
