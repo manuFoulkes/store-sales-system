4b117c06e255b9524917324bbeabfd8e6b15a29c
Manuel Foulkes <manuel.foulkes90@gmail.com>
2024-11-26 19:14:05 -0300

    Add Customer entity with attributes and reference to Sale entity (one-to-many)

---
+++ src/main/java/com/store/entity/Customer.java
@@ -0,0 +1,37 @@
+package com.store.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "customers")
+@Setter @Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString(exclude = "sales")
+@EqualsAndHashCode(exclude = "sales")
+@Builder
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    @Setter(AccessLevel.NONE)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "lastName", nullable = false)
+    private String lastName;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Sale> sales = new ArrayList<>();
+
+}
