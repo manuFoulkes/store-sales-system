a6ee3373842059c199357b20593efa30a231d073
Manuel Foulkes <manuel.foulkes90@gmail.com>
2024-11-27 11:20:01 -0300

    Implements getCustomerById in CustomerService

--- src/main/java/com/store/repository/CustomerRepository.java
+++ src/main/java/com/store/repository/CustomerRepository.java
@@ -4,5 +4,5 @@ import com.store.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
-    
+
 }
--- src/main/java/com/store/service/CustomerService.java
+++ src/main/java/com/store/service/CustomerService.java
@@ -1,8 +1,30 @@
 package com.store.service;
 
+import com.store.entity.Customer;
+import com.store.dto.customer.CustomerResponseDTO;
+import com.store.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CustomerService {
 
+    private final CustomerRepository customerRepository;
+
+    @Autowired
+    public CustomerService(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    public CustomerResponseDTO getCustomerById(Long id) {
+        Customer customer = customerRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Customer with id " + id + " not found"));
+
+        return new CustomerResponseDTO(
+                customer.getId(),
+                customer.getName(),
+                customer.getLastName(),
+                customer.getEmail()
+        );
+    }
 }
