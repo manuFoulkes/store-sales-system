5a0e44b233b0826609bf10f2a2cb38fb9994dc02
Manuel Foulkes <manuel.foulkes90@gmail.com>
2024-11-27 11:08:49 -0300

    Create CustomerController, CustomerService and CustomerRepository with basic structure

---
+++ src/main/java/com/store/controller/CustomerController.java
@@ -0,0 +1,19 @@
+package com.store.controller;
+
+import com.store.dto.customer.CustomerRequestDTO;
+import com.store.dto.customer.CustomerResponseDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/customers")
+public class CustomerController {
+
+}
---
+++ src/main/java/com/store/service/CustomerService.java
@@ -0,0 +1,8 @@
+package com.store.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerService {
+
+}
