7bbc41c5ba01e443052b4ccfe07df5b290cf25ab
Manuel Foulkes <manuel.foulkes90@gmail.com>
2024-11-29 11:06:49 -0300

    Add basic unit test for CustomerNotFoundException in getCustomerById

--- src/test/java/com/store/service/CustomerServiceTest.java
+++ src/test/java/com/store/service/CustomerServiceTest.java
@@ -2,8 +2,8 @@ package com.store.service;
 
 import com.store.dto.customer.CustomerResponseDTO;
 import com.store.entity.Customer;
+import com.store.exception.CustomerNotFoundException;
 import com.store.repository.CustomerRepository;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -13,6 +13,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -45,5 +46,17 @@ public class CustomerServiceTest {
         CustomerResponseDTO customerResponseDTO = customerService.getCustomerById(customerId);
 
         assertEquals(expectedCustomer.getName(), customerResponseDTO.name());
+    }
+
+    @Test
+    void getCustomerById_ShouldThrowAnException_WhenCustomerNotExist() {
+        // Arrange
+        Long nonExistingCustomerId = 1L;
+
+        when(customerRepository.findById(nonExistingCustomerId)).thenReturn(Optional.empty());
+
+        assertThrows(CustomerNotFoundException.class, () -> {
+            customerService.getCustomerById(nonExistingCustomerId);
+        });
     }
 }
