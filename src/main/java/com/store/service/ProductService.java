f2ccacdddb1a6f95ef169cc348b6185fecf8269e
Manuel Foulkes <manuel.foulkes90@gmail.com>
2024-12-06 16:27:28 -0300

    Implement ProductNotFoundException for getProductById method in ProductService

--- src/main/java/com/store/service/ProductService.java
+++ src/main/java/com/store/service/ProductService.java
@@ -2,6 +2,7 @@ package com.store.service;
 
 import com.store.dto.product.ProductResponseDTO;
 import com.store.entity.Product;
+import com.store.exception.product.ProductNotFoundException;
 import com.store.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,7 +19,7 @@ public class ProductService {
 
     public ProductResponseDTO getProductById(Long id) {
         Product product = productRepository.findById(id)
-                .orElseThrow(() -> new ProductNotFoundException("Producto with id " + id + " not found"));
+                .orElseThrow(() -> new ProductNotFoundException("Product with id " + id + " not found"));
 
         return new ProductResponseDTO(
                 product.getId(),
