3eb649635579ec4d7f6a635cc0c4014ce64b3b3e
Manuel Foulkes <manuel.foulkes90@gmail.com>
2024-12-06 16:19:29 -0300

    Implement getById method in ProductController

--- src/main/java/com/store/controller/ProductController.java
+++ src/main/java/com/store/controller/ProductController.java
@@ -1,7 +1,12 @@
 package com.store.controller;
 
+import com.store.dto.product.ProductResponseDTO;
 import com.store.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,5 +19,10 @@ public class ProductController {
     @Autowired
     public ProductController(ProductService productService) {
         this.productService = productService;
+    }
+
+    @GetMapping("{id}")
+    private ResponseEntity<ProductResponseDTO> getProductById(@PathVariable Long id) {
+        return new ResponseEntity<>(productService.getProductById(id), HttpStatus.FOUND);
     }
 }
