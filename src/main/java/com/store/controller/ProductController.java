0e7cc643214f06bbaf616738814ecc2946278dbb
Manuel Foulkes <manuel.foulkes90@gmail.com>
2024-12-06 16:16:21 -0300

    Add ProductController, ProductService and ProductRepository files

---
+++ src/main/java/com/store/controller/ProductController.java
@@ -0,0 +1,18 @@
+package com.store.controller;
+
+import com.store.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    private final ProductService productService;
+
+    @Autowired
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+}
---
+++ src/main/java/com/store/service/ProductService.java
@@ -0,0 +1,16 @@
+package com.store.service;
+
+import com.store.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    @Autowired
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+}
