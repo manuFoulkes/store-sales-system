fc6b83401e1533004443a54474ea3e50ca1342fd
Manuel Foulkes <manuel.foulkes90@gmail.com>
2024-12-06 17:35:06 -0300

    Implement getAllProducts method in ProductService

--- src/main/java/com/store/service/ProductService.java
+++ src/main/java/com/store/service/ProductService.java
@@ -7,6 +7,9 @@ import com.store.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class ProductService {
 
@@ -28,5 +31,27 @@ public class ProductService {
                 product.getPrice(),
                 product.getStock()
         );
+    }
+
+    public List<ProductResponseDTO> getAllProducts() {
+        List<Product> products = productRepository.findAll();
+
+        if(products.isEmpty()) {
+            throw new ProductNotFoundException("Products not found");
+        }
+
+        List<ProductResponseDTO> productResponseDTOList = new ArrayList<>();
+
+        for(Product product : products) {
+            ProductResponseDTO productResponseDTO = new ProductResponseDTO(
+                    product.getId(),
+                    product.getName(),
+                    product.getBrand(),
+                    product.getPrice(),
+                    product.getStock()
+            );
+            productResponseDTOList.add(productResponseDTO);
+        }
+        return productResponseDTOList;
     }
 }
