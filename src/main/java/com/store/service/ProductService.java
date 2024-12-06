0c5531f6e6417037157c7480959b8764d02c42b2
Manuel Foulkes <manuel.foulkes90@gmail.com>
2024-12-06 16:24:30 -0300

    Implement getProductById in ProductService with simple validation

--- src/main/java/com/store/service/ProductService.java
+++ src/main/java/com/store/service/ProductService.java
@@ -1,5 +1,7 @@
 package com.store.service;
 
+import com.store.dto.product.ProductResponseDTO;
+import com.store.entity.Product;
 import com.store.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,5 +14,18 @@ public class ProductService {
     @Autowired
     public ProductService(ProductRepository productRepository) {
         this.productRepository = productRepository;
+    }
+
+    public ProductResponseDTO getProductById(Long id) {
+        Product product = productRepository.findById(id)
+                .orElseThrow(() -> new ProductNotFoundException("Producto with id " + id + " not found"));
+
+        return new ProductResponseDTO(
+                product.getId(),
+                product.getName(),
+                product.getBrand(),
+                product.getPrice(),
+                product.getStock()
+        );
     }
 }
