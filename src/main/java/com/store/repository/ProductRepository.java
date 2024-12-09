4d5fbdbf2bca403fce4f43e6aa502802f6a19ce6
Manuel Foulkes <manuel.foulkes90@gmail.com>
2024-12-09 11:22:38 -0300

    Add findByNameAndBrand method in ProductRepository

--- src/main/java/com/store/repository/ProductRepository.java
+++ src/main/java/com/store/repository/ProductRepository.java
@@ -2,7 +2,14 @@ package com.store.repository;
 
 import com.store.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    @Query("SELECT p FROM Product p WHERE p.name = :name AND p.brand = :brand")
+    Optional<Product> findByNameAndBrand(@Param("name") String name, @Param("brand") String brand);
 
 }
