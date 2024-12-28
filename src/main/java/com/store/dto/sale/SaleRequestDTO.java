39f1ad6bac88a33ed5c07b12c2ae42e984f71665
Manuel Foulkes <manuel.foulkes90@gmail.com>
2024-12-28 19:52:11 -0300

    add SaleDetailRequestDTO

--- src/main/java/com/store/dto/sale/SaleRequestDTO.java
+++ src/main/java/com/store/dto/sale/SaleRequestDTO.java
@@ -1,6 +1,6 @@
 package com.store.dto.sale;
 
-import com.store.dto.saleDetail.SaleDetailRequestDTO.java;
+import com.store.dto.saleDetail.SaleDetailRequestDTO;
 import java.util.List;
 
 public record SaleRequestDTO(
---
+++ src/main/java/com/store/dto/saleDetail/SaleDetailRequestDTO.java
@@ -0,0 +1,4 @@
+package com.store.dto.saleDetail;
+
+public record SaleDetailRequestDTO() {
+}
