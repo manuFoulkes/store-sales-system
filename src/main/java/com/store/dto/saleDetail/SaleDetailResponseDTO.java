f4ef1325ccbfcd59a575080308065afd1e916213
Manuel Foulkes <manuel.foulkes90@gmail.com>
2024-12-28 20:46:07 -0300

    refactor SaleDetailRequestDTO and SaleDetailResponseDTO to include relevant fields

--- src/main/java/com/store/dto/saleDetail/SaleDetailResponseDTO.java
+++ src/main/java/com/store/dto/saleDetail/SaleDetailResponseDTO.java
@@ -2,8 +2,9 @@ package com.store.dto.saleDetail;
 
 import java.math.BigDecimal;
 
-public record SaleDetailRequestDTO(
+public record SaleDetailResponseDTO(
         Long productId,
+        String productName,
         int quantity,
         BigDecimal price
 ) {}
