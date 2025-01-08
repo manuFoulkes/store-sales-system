2adf5048e2c79d19b30791c4b55d4d607b8940b1
Manuel Foulkes <manuel.foulkes90@gmail.com>
2025-01-08 16:54:12 -0300

    implement countSalesByCustomerAndDate method in SaleRepository

--- src/main/java/com/store/repository/SaleRepository.java
+++ src/main/java/com/store/repository/SaleRepository.java
@@ -2,6 +2,13 @@ package com.store.repository;
 
 import com.store.entity.Sale;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+    @Query("SELECT COUNT(s) FROM Sale S WHERE s.customer.id = :customerId AND s.saleDate = :saleDate")
+    int countSalesByCustomerAndDate(@Param("customerId") Long customerId, @Param("saleDate")LocalDate saleDate);
 }
