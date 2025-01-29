d1f8ac66ec4494b30a8e12e313361ad0200219bf
Manuel Foulkes <manuel.foulkes90@gmail.com>
2025-01-29 19:10:12 -0300

    implement constructor with necessary fields in Product entity

--- src/main/java/com/store/entity/Product.java
+++ src/main/java/com/store/entity/Product.java
@@ -36,4 +36,10 @@ public class Product {
     @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<SaleDetail> saleDetails;
 
+    public Product(String name, String brand, double price, int stock) {
+        this.name = name;
+        this.brand = brand;
+        this.price = price;
+        this.stock = stock
+    }
 }
