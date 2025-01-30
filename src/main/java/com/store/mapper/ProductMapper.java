fd9b657ed07ff9e760c87ddf9d3f98152472e07c
Manuel Foulkes <manuel.foulkes90@gmail.com>
2025-01-30 19:53:31 -0300

    Implement toProduct method for mapping ProductRequestDTO to Product

--- src/main/java/com/store/mapper/ProductMapper.java
+++ src/main/java/com/store/mapper/ProductMapper.java
@@ -1,10 +1,14 @@
 package com.store.mapper;
 
+import com.store.dto.product.ProductRequestDTO;
 import com.store.dto.product.ProductResponseDTO;
 import com.store.entity.Product;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface ProductMapper {
+
     ProductResponseDTO toProductResponse(Product product);
+
+    Product toProduct(ProductRequestDTO productRequest);
 }
