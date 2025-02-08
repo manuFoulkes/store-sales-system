0767f0255e5283dc342abdedc185deff7d71aff2
Manuel Foulkes <manuel.foulkes90@gmail.com>
2025-02-08 19:36:24 -0300

    feat(exception): handle InsufficientStockException with 409 Conflict response

--- src/main/java/com/store/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/store/exception/handler/GlobalExceptionHandler.java
@@ -1,7 +1,26 @@
 package com.store.exception.handler;
 
+import com.store.exception.model.ApiError;
+import com.store.exception.product.InsufficientStockException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.Instant;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(InsufficientStockException.class)
+    public ResponseEntity<ApiError> handleInsufficientStock(InsufficientStockException ex) {
+        ApiError error = new ApiError(
+                HttpStatus.CONFLICT.value(),
+                "INSUFFICIENT_STOCK",
+                ex.getMessage(),
+                Instant.now()
+        );
+
+        return new ResponseEntity<>(error, HttpStatus.CONFLICT);
+    }
 }
