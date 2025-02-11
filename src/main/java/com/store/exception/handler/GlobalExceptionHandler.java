f66632e8e8301c28a0b372af4de83e58866af1e0
Manuel Foulkes <manuel.foulkes90@gmail.com>
2025-02-11 16:34:52 -0300

    feat(exception) handle ProductNotFoundException with 404 Not_Found

--- src/main/java/com/store/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/store/exception/handler/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.store.exception.handler;
 import com.store.exception.model.ApiError;
 import com.store.exception.product.InsufficientStockException;
 import com.store.exception.product.ProductAlreadyExistsException;
+import com.store.exception.product.ProductNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -28,7 +29,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(ProductAlreadyExistsException.class)
     public ResponseEntity<ApiError> handleProductAlreadyExist(ProductAlreadyExistsException ex) {
         ApiError error = new ApiError(
-                HttpStatus.CONTINUE.value(),
+                HttpStatus.CONFLICT.value(),
                 "PRODUCT_ALREADY_EXIST",
                 ex.getMessage(),
                 Instant.now()
@@ -37,5 +38,15 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(error, HttpStatus.CONFLICT);
     }
 
-    
+    @ExceptionHandler(ProductNotFoundException.class)
+    public ResponseEntity<ApiError> handleProductNotFound(ProductNotFoundException ex) {
+        ApiError error = new ApiError(
+                HttpStatus.NOT_FOUND.value(),
+                "PRODUCT_NOT_FOUND",
+                ex.getMessage(),
+                Instant.now()
+        );
+
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
 }
